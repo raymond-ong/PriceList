e4e01bbac9c46c617ca45b29324b4e5e454f0d24
raymond <remondo@gmail.com>
2023-12-29 00:47:09 +0800

    Base Code Hello World

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  View,
+  SafeAreaView, // To make sure we don't display anything on the notch of the phone
+  StyleSheet,
+  Text
+} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+        <Text>Hello World1</Text> 
+        <Text>Hello World2</Text> 
+        <Text>Hello World3</Text> 
+        <Text>Hello World4</Text> 
+        <Text>Hello World5</Text> 
+        <Text>Hello World6</Text> 
+        <Text>Hello World7</Text> 
+        <Text>Hello World8</Text> 
+        <Text>Hello World9</Text> 
+        <Text>Hello World10</Text> 
+        <Text>Hello World11</Text> 
+        <Text>Hello World12</Text> 
+        <Text>Hello World13</Text> 
+        <Text>Hello World14</Text> 
+        <Text>Hello World15</Text> 
+        <Text>Hello World16</Text> 
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App;
